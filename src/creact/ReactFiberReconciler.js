916cc6568c0453add20e985c9918560c55366c7a
unknown <416212316@qq.com>
2022-04-26 20:49:49 +0800

    ReactChildFiber

--- src/creact/ReactFiberReconciler.js
+++ src/creact/ReactFiberReconciler.js
@@ -1,6 +1,6 @@
-import { createFiber } from "./createFiber"
 import { renderHooks } from "./hooks"
-import { isArray, isStringOrNumber, Update, updateNode } from "./utils"
+import { reconcileChildren } from "./ReactChildFiber"
+import { updateNode } from "./utils"
 
 export function updateFunctionComponent(wip) {
     renderHooks(wip)
@@ -28,56 +28,3 @@ export function updateFragmentComponent(wip) {
     reconcileChildren(wip, wip.props.children)
 }
 
-function deleteChild(returnFiber, childToDelete) {
-    if(returnFiber.deletions) {
-        returnFiber.deletions.push(childToDelete)
-    }else{
-        returnFiber.deletions = [childToDelete]
-    }
-}
-
-function reconcileChildren(returnFiber, children) {
-    if(isStringOrNumber(children)) {
-        return
-    }
-    const newChildren = isArray(children) ? children : [children]
-
-    let previousNewFiber = null
-    let oldFiber = returnFiber.alternate && returnFiber.alternate.child
-    for(let i = 0; i < newChildren.length; i++) {
-        const newChild = newChildren[i]
-        if(newChild === null) {
-            continue
-        }
-        const newFiber = createFiber(newChild, returnFiber)
-        const same = sameNode(newFiber, oldFiber)
-        if(same) {
-            Object.assign(newFiber, {
-                alternate: oldFiber,
-                flags: Update,
-                stateNode: oldFiber.stateNode
-            })
-        }
-
-        if(!same && oldFiber) {
-            deleteChild(returnFiber, oldFiber)
-        }
-
-        if(oldFiber) {
-            oldFiber = oldFiber.sibling
-        }
-
-        if(previousNewFiber === null) {
-            returnFiber.child = newFiber
-        } else {
-            previousNewFiber.sibling = newFiber
-        }
-
-        previousNewFiber = newFiber
-    }
-
-}
-
-function sameNode(a, b) {
-    return a && b && a.key===b.key && a.type===b.type
-}
