12c526162c37312b95fa8c9da3b15409c75bb8aa
unknown <416212316@qq.com>
2022-05-18 22:58:11 +0800

    fix

--- src/creact-5-18/ReactFiberReconciler.js
+++ src/creact-5-18/ReactFiberReconciler.js
@@ -1,3 +1,13 @@
-export function updateHostComponent() {
+export function updateHostComponent(wip) {
+    // 更新自己
+    if(!wip.stateNode) {
+        wip.stateNode = document.createComment(wip.type)
+        // 属性
+    }
+    // 协调子节点
+    reconcileChildren(wip, wip.props.children)
+}
+
+function reconcileChildren(returnFiber, children) {
     
 }
