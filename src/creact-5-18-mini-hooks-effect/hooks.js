0cdb89535e52cd0daa1fabcc7523b9a0167296ef
unknown <416212316@qq.com>
2022-05-31 20:34:13 +0800

    fix

--- src/creact-5-18-mini-hooks-effect/hooks.js
+++ src/creact-5-18-mini-hooks-effect/hooks.js
@@ -1,8 +1,9 @@
 import { scheduleUpdateOnFiber } from "./ReactFiberWorkLoop"
-import { HookLayout, HookPassive } from "./utils"
+import { areHookInputsEqual, HookLayout, HookPassive } from "./utils"
 
 let currentlyRenderingFiber = null
 let workInProgressHook = null
+let currentHook = null
 
 export function renderHooks(wip) {
     currentlyRenderingFiber = wip
@@ -21,12 +22,15 @@ function updateWorkInProgressHook() {
         if(workInProgressHook) {
             // not head
             hook = workInProgressHook = workInProgressHook.next
+            currentHook = currentHook.next
         } else {
             // head hook
             hook = workInProgressHook = current.memorizedState
+            currentHook = current.memorizedState
         }
     } else {
         // 初始渲染
+        currentHook = null
         hook = {
             memorizedState: null,
             next: null
@@ -61,6 +65,16 @@ export function useReducer(reducer, initalState) {
 
 function updateEffectImp(hookFlags, create, deps) {
     const hook = updateWorkInProgressHook()
+    if(currentHook) {
+        // 检查 deps 是否变化
+        const prevEffect = currentHook.memorizedState
+        if(deps) {
+            const prevDeps = prevEffect.deps;
+            if(areHookInputsEqual(deps, prevDeps)) {
+                return
+            }
+        }
+    }
     const effect = {hookFlags, create, deps}
     hook.memorizedState = effect
 
